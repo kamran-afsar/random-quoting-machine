8e5df26b0a9b8d1e5070624954bf104edee3243b
Kamran Afsar <kamran.afsar@synergy-it.com>
2020-12-30 16:31:54 +0500

    added font awesome module

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Navbar } from './NavBar/Navbar';
 import { Provider, teamsTheme, teamsDarkTheme } from '@fluentui/react-northstar';
 import { RandomQuotes } from './Quotes/Quote';
 import "./App.css";
+import 'font-awesome/css/font-awesome.min.css';
 function App() {
   const [darkTheme, setDarkTheme] = React.useState(false);
   const handleTheme = (themeInput:boolean) => {
